5d47cb2a42639ab2873a58bc6ebaad0dec8c0e4c
David Garcia <garciasalgadodavid4@gmail.com>
2026-03-11 18:58:51 +0100

    Borramos viejo MovieServer (carpeta)

--- MovieServer/src/controllers/llm/index.js
+++
@@ -1,7 +0,0 @@
-// Importamos la función correcta que sí existe en nuestro modelo
-import { getMovieRecomendation } from "../../models/ollama/index.js";
-
-export async function llmCall(message) {
-    // Llamamos a la función que acabamos de importar
-    return getMovieRecomendation(message);
-}
--- MovieServer/src/index.js
+++
@@ -1,3 +0,0 @@
-import { startServer } from "./views/api/index.js"
-
-await startServer()
