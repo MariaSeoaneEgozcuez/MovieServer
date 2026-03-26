13125363d0318640e74582ce9ef5c0b9333c5182
MariaSeoane <jilari1212@gmail.com>
2026-03-26 17:48:21 +0100

    Añadir logout en index, funcionalidad en authcontrol, modificar bbdd para token revocados, authenticatetoken ahora mira tambien si el token es revocado

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,5 +1,6 @@
 // Middleware para manejar la autenticación con JWT
 import jwt from 'jsonwebtoken';
+import { isTokenRevoked } from '../models/User.js';
 
 // Middleware para verificar el token JWT (si existe o si es válido)
 export function authenticateToken(req, res, next) {
@@ -10,9 +11,13 @@ export function authenticateToken(req, res, next) {
         return res.status(401).json({ message: 'Acceso no autorizado' });
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, user) => {
         if (err) {
             return res.status(403).json({ message: 'Token inválido' });
+        }
+        const revoked = await isTokenRevoked(token);
+        if (revoked) {
+            return res.status(403).json({ message: 'Token revocado' });
         }
         req.user = user;
         next();
