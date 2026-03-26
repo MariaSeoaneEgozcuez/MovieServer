dd6a455ee6f84c983cb9f2771b4dfbbabd6cfa06
LindaFPayeras <lindafpayeraso@gmail.com>
2026-03-26 18:17:09 +0100

    cambios

--- src/controllers/telegram/bot.js
+++ src/controllers/telegram/bot.js
@@ -1,8 +1,8 @@
 import config from 'config';
 import { Telegraf } from 'telegraf';
-import { register, login } from '../controllers/authControl.js';
 
 const bot = new Telegraf(config.get("telegram.botToken"));
+const telegramId = ctx.from.id;
 
 bot.start((ctx) => {
     ctx.reply('Welcome ' + ctx.from.first_name + '! Lets search for movies you may like. \nFirst, please register or login to your account. \nUse /register or /login to get started.');
@@ -17,16 +17,17 @@ bot.settings((ctx) => {
 });
 
 bot.command('register', async (ctx) => {
-    ctx.reply("Register here:\nhttp://localhost:3000/api/auth/register");
+    ctx.reply("Register here:\nhttp://localhost:3000/api/auth/register?telegramId=" + telegramId);
+});
+
+
+bot.command('login', async (ctx) => {
+    ctx.reply("Login here:\nhttp://localhost:3000/api/auth/login?telegramId=" + telegramId);
 });
 
 bot.command('documentation', async (ctx) => {
     
     ctx.reply("Documentation here:\nhttp://localhost:3000/api/docs"); // Revisar
-});
-
-bot.command('login', async (ctx) => {
-    ctx.reply("Login here:\nhttp://localhost:3000/api/auth/login");
 });
 
 bot.on('text', async (ctx) => {
